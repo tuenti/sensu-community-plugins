d8dde4caccc14c3d4e37278e563929dc1ce2c89e
Jaime Soriano Pastor <jsoriano@tuenti.com>
2015-04-13 16:46:57 +0200

    Disable log in megacli calls

--- plugins/raid/check-raid.rb
+++ plugins/raid/check-raid.rb
@@ -83,7 +83,7 @@ class CheckRaid < Sensu::Plugin::Check::CLI
   def check_mega_raid
     # #YELLOW
     if File.exist?('/usr/sbin/megacli')  # rubocop:disable GuardClause
-      contents = `/usr/sbin/megacli -AdpAllInfo -aALL`
+      contents = `sudo /usr/sbin/megacli -AdpAllInfo -aALL -nolog`
       c = contents.lines.grep(/(Critical|Failed) Disks\s+\: 0/)
       # #YELLOW
       unless c.empty?   # rubocop:disable UnlessElse
