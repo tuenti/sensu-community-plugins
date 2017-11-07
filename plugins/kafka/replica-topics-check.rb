39b99187c8b770dc3573e290204078d1faabf7dd
Adrián del Campo <adelcampo@tuenti.com>
2017-11-07 18:38:27 +0100

    Fixed errors found in review

--- plugins/kafka/replica-topics-check.rb
+++ plugins/kafka/replica-topics-check.rb
@@ -1,44 +1,59 @@
+#! /usr/bin/env ruby
+#
+# check-replica-topics
+#
+# DESCRIPTION:
+#   This plugin checks replica topics properties.
+#
+# OUTPUT:
+#   plain-text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#
+# USAGE:
+#   ./check-replica-topics
+
 require 'sensu-plugin/check/cli'
 
 class ReplicaTopicCheck < Sensu::Plugin::Check::CLI
-	option :zookeeper,
-		description: 'ZooKeeper connect string',
-		short:       '-z NAME',
-		long:        '--zookeeper NAME',
-		default:     'kaf-01:2181'
+    option :zookeeper,
+        description: 'ZooKeeper connect string',
+        short:       '-z NAME',
+        long:        '--zookeeper NAME',
+        default:     'localhost:2181'
 
-	option :kafka_home,
-		description: 'Kafka home',
-		short:       '-k NAME',
-		long:        '--kafka-home NAME',
-		default:     '/opt/kafka'
+    option :kafka_home,
+        description: 'Kafka home',
+        short:       '-k NAME',
+        long:        '--kafka-home NAME',
+        default:     '/opt/kafka'
 
+    # gets the number of rows returned by the command
+    # @param cmd [String] the command to read the output from
+    def check_output(cmd)
+        lines = 0;
+        IO.popen(cmd + ' 2>&1') do |child|
+            lines = child.readlines().size
+        end
 
-	# read the output of a command
-	# @param cmd [String] the command to read the output from
-	def check_output(cmd)
-		lines = 0;
-		IO.popen(cmd + ' 2>&1') do |child|
-			lines = child.readlines().size
-		end
+        return lines
+    end
 
-		return lines
-	end
+    def run
+        kafka_topics_script = "#{config[:kafka_home]}/bin/kafka-topics.sh"
+        unknown "Can not find #{kafka_topics_script}" unless File.exist?(kafka_topics_script)
+        kafka_cmd = "#{kafka_topics_script} --zookeeper #{config[:zookeeper]} --describe"
 
-	def run
-		kafka_topics_script = "#{config[:kafka_home]}/bin/kafka-topics.sh --zookeeper #{config[:zookeeper]} --describe"
-		unknown "Can not find #{kafka_topics_script}" unless File.exist?(kafka_topics_script)	
+        unavailable_partitions = check_output("#{kafka_cmd} --unavailable-partitions")
+        critical "There are #{unavailable_partitions} partitions unavailable" if unavailable_partitions > 0
 
-		unavailable_partitions = check_output("#{kafka_topics_script} --unavailable-partitions")
-		if unavailable_partitions > 0 then
-			critical "There are #{unavailable_partitions} partitions unavailable"
-			exit 2
-		end
+        under_replica_partitions = check_output("#{kafka_cmd} --under-replicated-partitions")
+        warning "There are #{under_replica_partitions} partitions not replicated (and probably unavailable)" if under_replica_partitions > 0
 
-		under_replica_partitions = check_output("#{kafka_topics_script} --under-replicated-partitions")
-		if under_replica_partitions > 0 then
-			warning "There are #{under_replica_partitions} partitions not replicated (and probably unavailable)"
-			exit 1
-		end
-	end
+        ok if unavailable_partitions == 0 && under_replica_partitions == 0
+    end
 end
