83d97da263a8186cdb79b02e0475e8b42d0496fc
Adrián del Campo <adelcampo@tuenti.com>
2017-11-23 11:03:22 +0100

    Updated topic replication check to know which node is failing

--- plugins/kafka/check-topics-replication.rb
+++ plugins/kafka/check-topics-replication.rb
@@ -18,10 +18,13 @@
 #   ./check-replica-topics
 
 require 'sensu-plugin/check/cli'
+require 'zookeeper'
+require 'socket'
+require 'json'
 
 class ReplicaTopicCheck < Sensu::Plugin::Check::CLI
     option :zookeeper,
-        description: 'ZooKeeper connect string',
+        description: 'Zookeeper connect string',
         short:       '-z NAME',
         long:        '--zookeeper NAME',
         default:     'localhost:2181'
@@ -34,26 +37,43 @@ class ReplicaTopicCheck < Sensu::Plugin::Check::CLI
 
     # gets the number of rows returned by the command
     # @param cmd [String] the command to read the output from
-    def check_output(cmd)
-        lines = 0;
+    def get_misbehaving_nodes(cmd)
+        missing_leaders = Array.new
         IO.popen(cmd + ' 2>&1') do |child|
-            lines = child.readlines().size
+            lines = child.readlines()
+            lines.each() do |line|
+                leader = line.gsub!(/[\t ]*Topic:[\t ]*([a-zA-Z0-9\-\_]+)[\t ]*Partition:[\t ]*([0-9]+)[\t ]*Leader:[\t ]*([0-9]+)[\t ]*Replicas:[\t ]*([0-9,]+)[\t ]*Isr:[\t ]*([0-9,]+).*/, '\3')
+                if leader != nil
+                    missing_leaders.push(leader)
+                end
+            end
         end
 
-        return lines
+        return missing_leaders
     end
 
     def run
+        zk = Zookeeper.new(config[:zookeeper])
+        kafka_id = ''
+        hostname = Socket.gethostname
+
+        zk.get_children(path:'/brokers/ids')[:children].each { |id|
+            broker_info = zk.get(path:"/brokers/ids/#{id}")[:data]
+            json = JSON.parse(broker_info)
+
+            if json["host"] == hostname 
+                kafka_id = id
+            end
+        }
+
         kafka_topics_script = "#{config[:kafka_home]}/bin/kafka-topics.sh"
         unknown "Can not find #{kafka_topics_script}" unless File.exist?(kafka_topics_script)
         kafka_cmd = "#{kafka_topics_script} --zookeeper #{config[:zookeeper]} --describe"
 
-        unavailable_partitions = check_output("#{kafka_cmd} --unavailable-partitions")
-        critical "There are #{unavailable_partitions} partitions unavailable" if unavailable_partitions > 0
+        missing_leaders = get_misbehaving_nodes("#{kafka_cmd} --unavailable-partitions")
+        missing_leaders.concat(get_misbehaving_nodes("#{kafka_cmd} --under-replicated-partitions"))
+        critical "There are problems with leader #{hostname}. It's leader of a partition under-replicated / unavailable" if missing_leaders.include? kafka_id
 
-        under_replica_partitions = check_output("#{kafka_cmd} --under-replicated-partitions")
-        warning "There are #{under_replica_partitions} partitions not replicated (and probably unavailable)" if under_replica_partitions > 0
-
-        ok if unavailable_partitions == 0 && under_replica_partitions == 0
+        ok unless missing_leaders.include? kafka_id
     end
 end
